3a5b47f1df91c32e501fc2e466bc7e5787aade79
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-01-16 15:48:03 -0300

    Adicionando arquivos

--- src/server.js
+++ src/server.js
@@ -1,8 +1,4 @@
 import app from './app';
 
-const port = 3001;
-app.listen(port, () => {
-  console.log();
-  console.log(`Escutando na porta ${port}`);
-  console.log(`CTRL + Clique em http://localhost:${port}`);
-});
+const port = process.env.APP_PORT;
+app.listen(port);
